021fd7d87d8468b35fdd62ca862f3957e25c5b63
Sameer Saxena <59393128+Sampai28@users.noreply.github.com>
2021-01-20 09:53:20 +0530

    Add files via upload

---
+++ 0-1knapsack.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int knapsack(int* w,int* val, int n, int W){
+	int t[n+1][W+1];
+	for(int i = 0;i<n+1;i++){
+		for(int j = 0;j<W+1;j++){
+			if(i==0 || j==0){
+				t[i][j] = 0;
+			}
+		}
+	}
+	for(int i = 1;i<n+1;i++){
+		for(int j=1;j<W+1;j++){
+			if(w[i-1]<=j){
+				t[i][j] = std::max(val[i-1]+t[i-1][j-w[i-1]] , t[i-1][j] );
+			}
+			else{
+				t[i][j] = t[i-1][j];
+			}
+		}
+	}
+	return t[n][W];
+}
+
+int main(){
+	int wt[]= {10,20,30};
+	int val[] = {60,100,120};
+	int W = 50;
+	int n=3;
+	cout<<knapsack(wt,val,n,W);
+}
